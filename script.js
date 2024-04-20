c189576d16853dc9583815113e5d48d8ff1f9310
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-04-20 11:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// your code here
+let form = document.getElementById("form");
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+let button = document.getElementById("button");
+let url = document.getElementById("url");
+let URL = "https://localhost:8080/";
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let updateurl;
+    if (name.value !== "" && year.value !== "") {
+        updateurl = `${URL}?name=${name.value}&year=${year.value}`;
+    } else if (name.value === "" && year.value !== "") {
+        updateurl = `${URL}?year=${year.value}`;
+    } else if (name.value !== "" && year.value === "") {
+        updateurl = `${URL}?name=${name.value}`;
+    } else {
+        updateurl = `${URL}`;
+    }
+    url.textContent = updateurl;
+});
